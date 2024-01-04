36f022acd0628fbcc5cb6ae9af6f5dbaf615b6bc
Gautams21 <gs2788126@gmail.com>
2024-01-04 19:45:22 +0530

    edit

--- models/Schema.js
+++ models/Schema.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
+const validation= require('validator');
 const Schema=mongoose.Schema({
     name:{type:String,required:true},
-    email:{type:String,required:true,unique:true},
-    password:{type:String,required:true}
+    email:{type:String,required:true,unique:true,
+    validate(value){
+        if(!validation.isEmail(value)){
+            throw new Error("Email is Invalid!!!");
+        }
+    }},
+    password:{type:String,required:true,
+    validate(value){
+        if(!validation.isStrongPassword(value)){
+            throw new Error("Password is Invalid!!!");
+        }
+    }}
 })
 
 module.exports=mongoose.model('login',Schema);
